9969754631d8a827a43d0dbfef3377c468e1c845
eypkrky <karakayaeyp7@gmail.com>
2021-01-31 19:59:32 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GelAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId); // bu ürünleri kategoriye göre filtrele
+
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // IEntity implement eden class bir veritanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
